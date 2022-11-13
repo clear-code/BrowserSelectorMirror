e9b2dd499534a304f174acf462e1e7bcd07e36aa
Taketsugu YASHIRO <yashiro_taketsugu@yahoo.co.jp>
2022-11-13 20:34:31 +0900

    Add another test to pass, Modify HowToUse descriptions

--- script/BSVersionUp.HowToUse.md
+++ script/BSVersionUp.HowToUse.md
@@ -2,31 +2,46 @@
 
 「BSVersionUp」は、BrowserSelector のバージョンアップ用スクリプトである
 
-## バージョン情報の体系
+## 機能の概要
+
+- パラメータで指定したバージョン番号`$new_version` の値へ一括更新する
+- ProductCode と PackageCode の GUID を付け直す
+
+### バージョン情報の付与ルール
 
 - `vx.y.z`とする。先頭の「v」に続けて、0 以上の 3 つの整数 x y z を.（ピリオド）で区切って表記する
 - x,y,z の値域はいずれも 0-99（数字 1 桁または 2 桁）とする
 - 一部のファイルに、4 つの整数でバージョン情報を保持する行もある。4 つめは「0」固定とする
 
-## 機能説明
-
-- パラメータで指定したバージョン番号`$new_version` の値へ一括更新する
-- ProductCode と PackageCode の GUID を付け直す
-
-### 実行手順
+## 実行手順
 
 1. エクスプローラー上で`script` フォルダを右クリックして「PowerShell ウィンドウをここで開く」を選択する
 2. PowerShell ウィンドウで下記を実施する
 
 ```PowerShell
-~\browserselector\script> ./BSVersionUp.ps1 [$new_version]
+~\browserselector\script> .\BSVersionUp.ps1 [$new_version]
 ```
 
-- 引数なしで`./BSVersionUp.ps1` を実行した時は、リポジトリ内のバージョン情報の grep 結果を表示する
-- `$new_version` は 0 以上の 3 つの整数 x y z を.（ピリオド）で区切って指定する（例：`./BSVersionUp.ps1 2.2.4`）。「v」は不要
-- 指定した`$new_version` が「バージョン情報の体系」に合致しない場合、エラーとする
+- 引数なしで`.\BSVersionUp.ps1` を実行した時は、リポジトリ内のバージョン情報の grep 結果を表示する
+- `$new_version` は 0 以上の 3 つの整数 x y z を .（ピリオド）で区切って指定する（例：`.\BSVersionUp.ps1 2.2.4`）。「v」は不要
+- 指定した`$new_version` が上記「バージョン情報の付与ルール」に違反している場合、エラーとする
 
-### 注意事項
+### テストファイルの実行手順
 
-実行環境上で事前に PowerShell スクリプトの実行権限を付与しておくこと。詳細は<https://go.microsoft.com/fwlink/?LinkID=135170> を参照
+PowerShell のテスティングフレームワーク「[Pester](https://pester.dev/)」にて実行する。手順は次のとおり
 
+1. エクスプローラー上で`script` フォルダを右クリックして「PowerShell ウィンドウをここで開く」を選択する
+2. PowerShell ウィンドウで下記を実施する
+
+```PowerShell
+~\browserselector\script> Invoke-Pester
+```
+
+フォルダ内のテストファイル`BSVersionUp.Tests.ps1` が実行される
+
+- 上記テストは、`script` 以外のパスから Invoke すると一部"Failed" となる（`script` を基準として更新対象ファイルの存在チェックを行っているため）
+
+### 実行前・実行後の注意事項
+
+- 実行環境上で事前に PowerShell スクリプトの実行権限を付与しておくこと。詳細は<https://go.microsoft.com/fwlink/?LinkID=135170> を参照
+- 当スクリプトによって変更したファイルのコミットの前に、diff を確認すること
--- script/BSVersionUp.Tests.ps1
+++ script/BSVersionUp.Tests.ps1
@@ -1,46 +1,48 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-Import-Module "$here\$sut" -Force
+﻿# Invoke these tests at '\browserselector\script' 
+
+Describe "Check if the current version is proper" {
+    Context "Compare the extracted current version to the expected" {
+        $result = .\BSVersionUp.ps1
+        It "Validity of the current version" {
+            # Be sure to update this number according to the version it should be
+            $result | Should Match "2.2.3"
+            # 
+        }
+    }
+}
 
 Describe "Target files to be existent" {
     Context "For .vdproj file" {
-        It "Exist" {
+        It "Exist BrowserSelectorSetup.vdproj" {
             "../BrowserSelectorSetup/BrowserSelectorSetup.vdproj" | Should Exist
         } 
     }
     Context "For .rc file" {
-        It "Exist" {
+        It "Exist BrowserSelector.rc" {
             "../BrowserSelector/BrowserSelector.rc" | Should Exist
         } 
-        It "Exist" {
+        It "Exist BrowserSelectorBHO.rc" {
             "../BrowserSelectorBHO/BrowserSelectorBHO.rc" | Should Exist
         } 
-        It "Exist" {
+        It "Exist BrowserSelectorTalk.rc" {
             "../BrowserSelectorTalk/BrowserSelectorTalk.rc" | Should Exist
         } 
     }
 }
-Describe "BSVersionUp" {
-#    Context "Extract-current_version" {
-#        It "Contain" {
-#            "./testfiles/testfile.vdproj" | Should Contain "2.2.3"
-#        } 
-#        It "For .rc testfile" {
-#            "./testfiles/testfile.rc" | Should Contain "2.2.3"
-#        } 
-#    }
-    Context "Parameters to fail" {
-        It "more than 2 digits" {
-            {BSVersionUp -new_version "0.0.100"} | Should Throw
+
+Describe "Other BSVersionUp tests" {
+    Context "Parameters to fail would include " {
+        It "One that consists of more than 2 digits" {
+            {.\BSVersionUp.ps1 -new_version 0.0.100} | Should Throw
         } 
-        It "negative number" {
-            {BSVersionUp -new_version "-1.0.0"} | Should Throw
+        It "Negative number" {
+            {.\BSVersionUp -new_version -1.0.0} | Should Throw
         } 
-        It "more than 3 numbers" {
-            {BSVersionUp -new_version "9.8.7.6"} | Should Throw
+        It "More than 3 numbers in total" {
+            {.\BSVersionUp -new_version 9.8.7.6} | Should Throw
         } 
-        It "separator is not period" {
-            {BSVersionUp -new_version "1,2,3"} | Should Throw
+        It "Any invalid separator other than period" {
+            {.\BSVersionUp -new_version 1,2,3} | Should Throw
         } 
     }
 }
