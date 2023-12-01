0acfb0f2d37f59ec1120b558da113a961b1de4bb
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2023-12-01 08:03:20 +0000

    Set output mode of DummyTalker as binary

--- BrowserSelectorTalk/DummyTalker.c
+++ BrowserSelectorTalk/DummyTalker.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
 int error(char *msg)
 {
@@ -18,6 +19,9 @@ int error(char *msg)
 
 int main(int argc, char *argv[])
 {
+    /* Prevent Windows from corrupting data */
+    setmode(_fileno(stdout), O_BINARY);
+
     char *url;
     int len;
 
