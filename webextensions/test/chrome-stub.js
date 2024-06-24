8c99a7a614d3d18cf9aeca221110e2610f2040f3
Takuro Ashie <ashie@clear-code.com>
2024-06-24 21:57:12 +0900

    Started to adding unit tests for WebExtensions add-ons

---
+++ webextensions/test/chrome-stub.js
@@ -0,0 +1,50 @@
+global.chrome = {
+    alarms: {
+	create: function() {
+	},
+	onAlarm: {
+	    addListener: function() {
+	    }
+	},
+    },
+    runtime: {
+	sendNativeMessage: function() {
+	},
+    },
+    tabs: {
+	onCreated: {
+	    addListener: function() {
+	    }
+	},
+	onRemoved: {
+	    addListener: function() {
+	    }
+	},
+	onUpdated: {
+	    addListener: function() {
+	    }
+	},
+    },
+    windows: {
+	onCreated: {
+	    addListener: function() {
+	    }
+	},
+	onRemoved: {
+	    addListener: function() {
+	    }
+	},
+    },
+    webRequest: {
+	onBeforeRequest: {
+	    addListener: function() {
+	    },
+	},
+    },
+    webNavigation: {
+	onCommitted: {
+	    addListener: function() {
+	    }
+	},
+    },
+};
